fbee571b2d158381e7590ffe7b432b7e91e39959
Nurizki <nutrisi1udin@gmail.com>
2022-10-02 19:23:06 +0700

    tugas 2 js
    tugas niomic

---
+++ tugas2.js
@@ -0,0 +1,15 @@
+var a=1;
+var b=1.5;
+var c="dua";
+var d=true;
+var e=null;
+var f;
+var g = [1,2,3,4,5,6];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
